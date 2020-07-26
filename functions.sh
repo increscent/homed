fc19c4ac25124d0e4d0b1f7adb79e82384c051f0
Robert Williams <robert@increscent.org>
2020-07-25 22:46:09 -0600

    Added support for multiple sync folders

--- functions.sh
+++ functions.sh
@@ -6,55 +6,46 @@ homed=$4
 prev_time=$5
 cur_time=$6
 
-if [ -z "$id" ]
+homed_id="$homed_id"
+homed_local="$homed/local/$(basename "$dir")"
+
+if [ -z "$id" ] || [ -z "$dir" ] || [ -z "$homed" ] || [ -z "$homed_id" ] || [ -z "$homed_local" ]
 then
     echo "Sync failed: no uuid"
-    exit 1
-fi
-
-if [ -z "$dir" ]
-then
-    echo "Sync failed: no dir"
-    exit 1
-fi
-
-if [ -z "$homed" ]
-then
-    echo "Sync failed: no homed"
     exit 1
 fi
 
 case "$1" in
 
 prepare-sync)
-    if [ -f "$homed/local/lock" ]
+    if [ -f "$homed_local/lock" ]
     then
         echo "locked"
         exit 0
     fi
 
-    touch "$homed/local/lock"
+    touch "$homed_local/lock"
 
-    mkdir -p "$homed/$id" "$homed/local" "$dir"
+    mkdir -p "$homed_id" "$homed_local" "$dir"
 
-    find "$dir" -printf "%P\t%Ts\t%s\t%y\n" | LC_ALL=C sort > "$homed/$id/branch_tmp.txt"
+    find "$dir" -printf "%P\t%Ts\t%s\t%y\n" | LC_ALL=C sort > "$homed_id/branch_tmp.txt"
 
-    cp -n "$homed/$id/branch_tmp.txt" "$homed/local/base.txt"
+    cp -n "$homed_id/branch_tmp.txt" "$homed_local/base.txt"
 
-    awk -f "$homed/awk/create-branch.awk" -v dir="$dir" -v cur_time="$cur_time" "$homed/local/base.txt" "$homed/$id/branch_tmp.txt" > "$homed/$id/branch.txt"
+    awk -f "$homed/awk/create-branch.awk" -v dir="$dir" -v cur_time="$cur_time" "$homed_local/base.txt" "$homed_id/branch_tmp.txt" > "$homed_id/branch.txt"
 
-    awk -v prev_time="$prev_time" 'BEGIN {FS = "\t"} $5 > prev_time' "$homed/$id/branch.txt" > "$homed/$id/additions.txt"
+    awk -v prev_time="$prev_time" 'BEGIN {FS = "\t"} $5 > prev_time' "$homed_id/branch.txt" > "$homed_id/additions.txt"
 
-    awk -f "$homed/awk/find-deletions.awk" -v cur_time="$cur_time" "$homed/local/base.txt" "$homed/$id/branch.txt" > "$homed/$id/deletions_tmp.txt"
-    awk -f "$homed/awk/merge-deletions.awk" "$homed/$id/deletions_tmp.txt" "$homed/local/deletions.txt" > "$homed/$id/deletions_tmp1.txt"
-    awk -f "$homed/awk/prune-deletions.awk" "$homed/$id/branch.txt" "$homed/$id/deletions_tmp1.txt" > "$homed/$id/pruned_deletions.txt"
+    awk -f "$homed/awk/find-deletions.awk" -v cur_time="$cur_time" "$homed_local/base.txt" "$homed_id/branch.txt" > "$homed_id/deletions_tmp.txt"
+    awk -f "$homed/awk/merge-deletions.awk" "$homed_id/deletions_tmp.txt" "$homed_local/deletions.txt" > "$homed_id/deletions_tmp1.txt"
+    awk -f "$homed/awk/prune-deletions.awk" "$homed_id/branch.txt" "$homed_id/deletions_tmp1.txt" > "$homed_id/pruned_deletions.txt"
 
-    awk -v prev_time="$prev_time" 'BEGIN {FS = "\t"} $5 > prev_time' "$homed/$id/pruned_deletions.txt" > "$homed/$id/deletions.txt"
+    awk -v prev_time="$prev_time" 'BEGIN {FS = "\t"} $5 > prev_time' "$homed_id/pruned_deletions.txt" > "$homed_id/deletions.txt"
 
-    mkdir -p "$homed/$id/remote"
+    mkdir -p "$homed_id/remote"
 
-    modifications=$(awk -v prev_time="$prev_time" 'BEGIN {FS = "\t"} $5 > prev_time || $2 > prev_time' "$homed/$id/branch.txt")
-    deletions=$(awk -v prev_time="$prev_time" 'BEGIN {FS = "\t"} $5 > prev_time' "$homed/$id/pruned_deletions.txt")
+    modifications=$(awk -v prev_time="$prev_time" 'BEGIN {FS = "\t"} $5 > prev_time || $2 > prev_time' "$homed_id/branch.txt")
+    deletions=$(awk -v prev_time="$prev_time" 'BEGIN {FS = "\t"} $5 > prev_time' "$homed_id/pruned_deletions.txt")
 
     if [ -z "$modifications" ] && [ -z "$deletions" ]
     then
@@ -66,28 +57,28 @@ prepare-sync)
 # copy additions and deletions
 
 copy-and-delete)
-    awk -f "$homed/awk/merge-deletions.awk" "$homed/$id/pruned_deletions.txt" "$homed/$id/remote/deletions.txt" > "$homed/$id/combined_deletions.txt"
-    awk -f "$homed/awk/prune-deletions.awk" "$homed/$id/branch.txt" "$homed/$id/combined_deletions.txt" > "$homed/$id/pruned_deletions.txt"
-    awk -f "$homed/awk/prune-deletions.awk" "$homed/$id/remote/additions.txt" "$homed/$id/pruned_deletions.txt" > "$homed/local/deletions.txt"
+    awk -f "$homed/awk/merge-deletions.awk" "$homed_id/pruned_deletions.txt" "$homed_id/remote/deletions.txt" > "$homed_id/combined_deletions.txt"
+    awk -f "$homed/awk/prune-deletions.awk" "$homed_id/branch.txt" "$homed_id/combined_deletions.txt" > "$homed_id/pruned_deletions.txt"
+    awk -f "$homed/awk/prune-deletions.awk" "$homed_id/remote/additions.txt" "$homed_id/pruned_deletions.txt" > "$homed_local/deletions.txt"
 
-    awk -f "$homed/awk/find-additions.awk" "$homed/$id/branch.txt" "$homed/$id/remote/additions.txt" > "$homed/$id/additions_tmp.txt"
-    awk -f "$homed/awk/prune-additions.awk" "$homed/local/deletions.txt" "$homed/$id/additions_tmp.txt" > "$homed/$id/additions.txt"
-    awk -f "$homed/awk/pair-additions.awk" "$homed/$id/branch.txt" "$homed/$id/additions.txt" | LC_ALL=C sort > "$homed/$id/paired_additions.txt"
-    awk -f "$homed/awk/copy-additions.awk" -v dir="$dir" "$homed/$id/paired_additions.txt" "$homed/local/deletions.txt" > "$homed/$id/copy_additions.txt"
-    cat "$homed/$id/copy_additions.txt" | xargs -0 -I {} bash -c '{}'
+    awk -f "$homed/awk/find-additions.awk" "$homed_id/branch.txt" "$homed_id/remote/additions.txt" > "$homed_id/additions_tmp.txt"
+    awk -f "$homed/awk/prune-additions.awk" "$homed_local/deletions.txt" "$homed_id/additions_tmp.txt" > "$homed_id/additions.txt"
+    awk -f "$homed/awk/pair-additions.awk" "$homed_id/branch.txt" "$homed_id/additions.txt" | LC_ALL=C sort > "$homed_id/paired_additions.txt"
+    awk -f "$homed/awk/copy-additions.awk" -v dir="$dir" "$homed_id/paired_additions.txt" "$homed_local/deletions.txt" > "$homed_id/copy_additions.txt"
+    cat "$homed_id/copy_additions.txt" | xargs -0 -I {} bash -c '{}'
 
-    awk -f "$homed/awk/validate-deletions.awk" "$homed/$id/branch.txt" "$homed/local/deletions.txt" > "$homed/$id/deletions.txt"
-    awk 'BEGIN {FS = "\t"} {print $1}' "$homed/$id/deletions.txt" | xargs -r -I {} rm -rf "$dir/{}"
+    awk -f "$homed/awk/validate-deletions.awk" "$homed_id/branch.txt" "$homed_local/deletions.txt" > "$homed_id/deletions.txt"
+    awk 'BEGIN {FS = "\t"} {print $1}' "$homed_id/deletions.txt" | xargs -r -I {} rm -rf "$dir/{}"
     ;;
 
 # rsync
 
 cleanup-and-reset)
-    find "$dir" -printf "%P\t%Ts\t%s\t%y\n" | LC_ALL=C sort > "$homed/$id/base.txt"
-    awk -f "$homed/awk/create-branch.awk" -v dir="$dir" -v cur_time="$cur_time" "$homed/$id/branch.txt" "$homed/$id/base.txt" > "$homed/local/base.txt"
-    rm -r "$homed/$id"
+    find "$dir" -printf "%P\t%Ts\t%s\t%y\n" | LC_ALL=C sort > "$homed_id/base.txt"
+    awk -f "$homed/awk/create-branch.awk" -v dir="$dir" -v cur_time="$cur_time" "$homed_id/branch.txt" "$homed_id/base.txt" > "$homed_local/base.txt"
+    rm -r "$homed_id"
 
-    rm "$homed/local/lock"
+    rm "$homed_local/lock"
     ;;
 
 esac
--- sync.sh
+++ sync.sh
@@ -1,21 +1,13 @@
 #!/bin/bash
 
-# things to add:
-#   - abort if unchanged
-#   - backup directory (local or remote) where files are never deleted
-#   - kick off run for other sync if changes are found
-
 source "$1"
 
 id=$(uuidgen)
 prev_time=0
 cur_time=$(date +%s)
 
-# homed/local/deletions.txt         saved deletions for previous/future run
-# homed/$id/deletions.txt           working deletions for current run
-# homed/$id/pruned_deletions.txt    working pruned deletions for current run
-
-if [ -z "$id" ] || [ -z "$host" ] || [ -z "$alias" ] || [ -z "$local_dir" ] || [ -z "$remote_dir" ] || [ -z "$local_homed" ] || [ -z "$remote_homed" ]
+# Make sure variables are set
+if [ -z "$id" ] || [ -z "$host" ] || [ -z "$alias" ] || [ -z "$local_dirs" ] || [ -z "$remote_dirs" ] || [ -z "$local_homed" ] || [ -z "$remote_homed" ]
 then
     echo "Sync failed: missing variables"
     exit 1
@@ -27,6 +19,8 @@ then
 fi
 
 echo "prev_time = $prev_time"
+echo "cur_time = $cur_time"
+echo "date = $(date)"
 
 ssh $host bash -c "uname -a" > /dev/null
 
@@ -38,56 +32,77 @@ fi
 
 cd "$local_homed"
 
-echo "Prepare sync -- local"
-local_prepare_result=$("$local_homed/functions.sh" 'prepare-sync' "$id" "$local_dir" "$local_homed" "$prev_time" "$cur_time")
+for index in ${!local_dirs[*]}
+do
+    local_dir=${local_dirs[$index]}
+    remote_dir=${remote_dirs[$index]}
 
-if [ "$local_prepare_result" = 'locked' ]
-then
-    exit 1
-fi
+    # Make sure variables are set
+    if [ -z "$local_dir" ] || [ -z "$remote_dir" ]
+    then
+        echo "Sync failed: missing variables"
+        exit 1
+    fi
 
-echo "Prepare sync -- remote"
-remote_prepare_result=$(ssh $host "\"$remote_homed/functions.sh\" 'prepare-sync' \"$id\" \"$remote_dir\" \"$remote_homed\" \"$prev_time\" \"$cur_time\"")
+    echo "Prepare sync -- local"
+    local_prepare_result=$("$local_homed/functions.sh" 'prepare-sync' "$id" "$local_dir" "$local_homed" "$prev_time" "$cur_time")
 
-if [ "$remote_prepare_result" = 'locked' ]
-then
-    echo "Cleanup and reset -- local"
-    "$local_homed/functions.sh" 'cleanup-and-reset' "$id" "$local_dir" "$local_homed" "$prev_time" "$cur_time"
-    exit 1
-fi
+    if [ "$local_prepare_result" = 'locked' ]
+    then
+        echo "Sync failed: local locked"
+        exit 1
+    fi
 
-if [ "$local_prepare_result" = 'unchanged' ] && [ "$remote_prepare_result" = 'unchanged' ]
-then
+    echo "Prepare sync -- remote"
+    remote_prepare_result=$(ssh $host "\"$remote_homed/functions.sh\" 'prepare-sync' \"$id\" \"$remote_dir\" \"$remote_homed\" \"$prev_time\" \"$cur_time\"")
+
+    if [ "$remote_prepare_result" = 'locked' ]
+    then
+        echo "Cleanup and reset -- local"
+        "$local_homed/functions.sh" 'cleanup-and-reset' "$id" "$local_dir" "$local_homed" "$prev_time" "$cur_time"
+
+        echo "Sync failed: remote locked"
+        exit 1
+    fi
+
+    if [ "$local_prepare_result" = 'unchanged' ] && [ "$remote_prepare_result" = 'unchanged' ]
+    then
+        echo "Cleanup and reset -- local"
+        "$local_homed/functions.sh" 'cleanup-and-reset' "$id" "$local_dir" "$local_homed" "$prev_time" "$cur_time"
+
+        echo "Cleanup and reset -- remote"
+        ssh $host "\"$remote_homed/functions.sh\" 'cleanup-and-reset' \"$id\" \"$remote_dir\" \"$remote_homed\" \"$prev_time\" \"$cur_time\""
+
+        echo "Sync failed: no changes"
+        exit 1
+    fi
+
+    scp $host:"$remote_homed/$id/additions.txt" "$local_homed/$id/remote/additions.txt"
+    scp $host:"$remote_homed/$id/deletions.txt" "$local_homed/$id/remote/deletions.txt"
+    scp "$local_homed/$id/additions.txt" $host:"$remote_homed/$id/remote/additions.txt"
+    scp "$local_homed/$id/deletions.txt" $host:"$remote_homed/$id/remote/deletions.txt"
+
+    exit 0
+
+    echo "Copy and delete -- local"
+    "$local_homed/functions.sh" 'copy-and-delete' "$id" "$local_dir" "$local_homed" "$prev_time" "$cur_time"
+
+    echo "Copy and delete -- remote"
+    ssh $host "\"$remote_homed/functions.sh\" 'copy-and-delete' \"$id\" \"$remote_dir\" \"$remote_homed\" \"$prev_time\" \"$cur_time\""
+
+    echo "rsync -- local -> remote"
+    rsync -uavz "$local_dir/" $host:"$remote_dir"
+
+    echo "rsync -- remote -> local"
+    rsync -uavz $host:"$remote_dir/" "$local_dir"
+
     echo "Cleanup and reset -- local"
     "$local_homed/functions.sh" 'cleanup-and-reset' "$id" "$local_dir" "$local_homed" "$prev_time" "$cur_time"
 
     echo "Cleanup and reset -- remote"
     ssh $host "\"$remote_homed/functions.sh\" 'cleanup-and-reset' \"$id\" \"$remote_dir\" \"$remote_homed\" \"$prev_time\" \"$cur_time\""
-    exit 1
-fi
 
-scp $host:"$remote_homed/$id/additions.txt" "$local_homed/$id/remote/additions.txt"
-scp $host:"$remote_homed/$id/deletions.txt" "$local_homed/$id/remote/deletions.txt"
-scp "$local_homed/$id/additions.txt" $host:"$remote_homed/$id/remote/additions.txt"
-scp "$local_homed/$id/deletions.txt" $host:"$remote_homed/$id/remote/deletions.txt"
-
-echo "Copy and delete -- local"
-"$local_homed/functions.sh" 'copy-and-delete' "$id" "$local_dir" "$local_homed" "$prev_time" "$cur_time"
-
-echo "Copy and delete -- remote"
-ssh $host "\"$remote_homed/functions.sh\" 'copy-and-delete' \"$id\" \"$remote_dir\" \"$remote_homed\" \"$prev_time\" \"$cur_time\""
-
-echo "rsync -- local -> remote"
-rsync -uavz "$local_dir/" $host:"$remote_dir"
-
-echo "rsync -- remote -> local"
-rsync -uavz $host:"$remote_dir/" "$local_dir"
-
-echo "Cleanup and reset -- local"
-"$local_homed/functions.sh" 'cleanup-and-reset' "$id" "$local_dir" "$local_homed" "$prev_time" "$cur_time"
-
-echo "Cleanup and reset -- remote"
-ssh $host "\"$remote_homed/functions.sh\" 'cleanup-and-reset' \"$id\" \"$remote_dir\" \"$remote_homed\" \"$prev_time\" \"$cur_time\""
+done
 
 echo $cur_time > "$local_homed/local/prev_time_$alias.txt"
 
